814f6bc0a25c36c47491e4bd4cd21c93a9cd386c
Håkon Vågsether <hauk142@gmail.com>
2013-03-25 01:19:28 +0100

    Added a class hierarchy

--- src/main.py
+++ src/main.py
@@ -1,14 +1,48 @@
 import pyglet
 
 
+window = pyglet.window.Window(800, 600, caption="Pheromone")
+window.clear()
+
 pyglet.resource.path = ["../res"]
 pyglet.resource.reindex()
 
-antIdle = pyglet.resource.image("ants/idle.png")
+class Drawable(object):
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
 
 
-if __name__ == '__main__':
-	window = pyglet.window.Window(800, 600, caption="Pheromone")
-	window.clear()
+class AnimateDrawable(Drawable):
+	def __init__(self, animation, x, y):
+		super(AnimateDrawable, self).__init(self, x, y)
+		self.animation = animation
+	
 
-	pyglet.app.run()
+class InanimateDrawable(Drawable):
+	def __init__(self, image, x, y):
+		super(InanimateDrawable, self).__init(self, x, y)
+		self.image = image
+
+
+class Ant(AnimateDrawable):
+	def __init__(self, x, y):
+		super(Ant, self).__init__(self, animation, x, y)
+
+
+class Debris(InanimateDrawable):
+	def __init__(self, x, y): 
+		super(Debris, self).__init__(self, image, x, y)
+
+
+
+def main():
+	print "Welcome to Pheromone!"
+	print "Pheromone is an ant colony simulator."
+
+if __name__ == "__main__":
+	main()
+
+
+# move
+pyglet.app.run()
