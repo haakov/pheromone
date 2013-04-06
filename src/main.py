83ba0898e75843c5c37d5bd698cc133914c800a4
Håkon Vågsether <hauk142@gmail.com>
2013-04-06 02:28:18 +0200

    Structure tidying

--- src/main.py
+++ src/main.py
@@ -18,34 +18,37 @@ class Drawable(object):
 
 class AnimateDrawable(Drawable):
 	def __init__(self, animation, x, y):
-		super(AnimateDrawable, self).__init(self, x, y)
+		super(AnimateDrawable, self).__init__(x, y)
 		self.animation = animation
 
 class InanimateDrawable(Drawable):
 	def __init__(self, image, x, y):
-		super(InanimateDrawable, self).__init(self, x, y)
+		super(InanimateDrawable, self).__init__(x, y)
 		self.image = image
 
 
 class Ant(AnimateDrawable):
 	def __init__(self, x, y):
-		super(Ant, self).__init__(self, animation, x, y)
+		super(Ant, self).__init__(animation, x, y)
 
 class Debris(InanimateDrawable):
 	def __init__(self, x, y): 
-		super(Debris, self).__init__(self, image, x, y)
+		super(Debris, self).__init__(image, x, y)
 
 class Food(InanimateDrawable):
 	def __init__(self, x, y):
-		super(Food, self).__init__(self, image, x, y)
+		super(Food, self).__init__(image, x, y)
 
 class Nest(InanimateDrawable): 
 	def __init__(self):
-		# The Nest image can't be outside the screen. The Nest graphics will be made soon.
-		# x = randrange(0, screenWidth - baseWidth)
-		# y = randrange(0, screenHeight - baseHeight)
+		self.image = pyglet.image.load("res/ants/nest.png") # TODO: Whether to use image or resource here
+		self.width, self.height = self.image.width, self.image.height
+		
+		self.x = randrange(0, screenWidth - self.width)
+		self.y = randrange(0, screenHeight - self.height)
+		print self.x, self.y
 
-		super(Nest, self).__init__(self, image, x, y)
+		super(Nest, self).__init__(self.image, self.x, self.y)
 
 
 @window.event
