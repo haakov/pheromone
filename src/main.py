3c28e6187b5792a133eb3ba697eb76923d7a09af
hauk142 <hauk142@gmail.com>
2013-04-18 22:36:27 +0200

    no time to write commit message

--- src/main.py
+++ src/main.py
@@ -15,7 +15,13 @@ class Ant(object):
 
 class Debris(object):
 	def __init__(self): 
-		pass
+		self.image = pyglet.image.load("res/debris/" + str(randrange(1, 3)) + ".png")
+		self.width, self.height = self.image.width, self.image.height
+
+		self.x = randrange(0, screenWidth - self.width)
+		self.y = randrange(0, screenHeight - self.height)
+
+		self.sprite = pyglet.sprite.Sprite(self.image, self.x, self.y)
 
 class Food(object):
 	def __init__(self):
@@ -31,6 +37,11 @@ class Nest(object):
 
 		self.sprite = pyglet.sprite.Sprite(self.image, self.x, self.y)
 home = Nest()
+debris = Debris()
+debris1 = Debris()
+debris2 = Debris()
+debris3 = Debris()
+debris4 = Debris()
 
 @window.event
 def update(self):
@@ -50,6 +61,10 @@ if __name__ == "__main__":
 def on_draw():
 	window.clear()
 	home.sprite.draw()
-
+	debris.sprite.draw()
+	debris1.sprite.draw()
+	debris2.sprite.draw()
+	debris3.sprite.draw()
+	debris4.sprite.draw()
 # TODO: move this somewhere else
 pyglet.app.run()
