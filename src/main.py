24d529bca267a8653cfb75373fef348b38c78ce9
hauk142 <hauk142@gmail.com>
2013-04-22 14:32:51 +0200

    Added Ants, drawn facing outwards from the Nest. Current TODO is to make sure nothing is drawn off screen, add collision detection and food. Also, the ant sprite is colossal, so it might be an idea to shrink it.

--- src/main.py
+++ src/main.py
@@ -11,11 +11,20 @@ pyglet.resource.reindex()
 
 class Ant(object):
 	def __init__(self):
-		pass
+		self.image = pyglet.resource.image("ants/idle.png")
+		self.width, self.height = self.image.width, self.image.height
+		
+		self.x = randrange(home.x-self.width, home.x+home.width)
+		self.y = randrange(home.y-self.height, home.y+home.height)
+		# Flip if the ant if facing the other way
+		if (self.x+self.width/2)<(home.x+home.width/2):
+			self.image = pyglet.resource.image("ants/idle.png", flip_x=True)
+
+		self.sprite = pyglet.sprite.Sprite(self.image, self.x, self.y)
 
 class Debris(object):
 	def __init__(self): 
-		self.image = pyglet.image.load("res/debris/" + str(randrange(1, 3)) + ".png")
+		self.image = pyglet.resource.image("debris/" + str(randrange(1, 3)) + ".png")
 		self.width, self.height = self.image.width, self.image.height
 		while True:
 			self.x = randrange(0, screenWidth - self.width)
@@ -33,7 +42,7 @@ class Food(object):
 
 class Nest(object): 
 	def __init__(self):
-		self.image = pyglet.image.load("res/ants/nest.png") # TODO: Whether to use image or resource here
+		self.image = pyglet.resource.image("ants/nest.png") # TODO: Whether to use image or resource here
 		self.width, self.height = self.image.width, self.image.height
 		
 		self.x = randrange(0, screenWidth - self.width)
@@ -43,6 +52,10 @@ class Nest(object):
 home = Nest()
 
 debrises = []
+ants = []
+
+for i in range(0,8):
+	ants.append(Ant())
 
 for i in range(0, 255):
 	debrises.append(Debris())
@@ -66,6 +79,8 @@ def on_draw():
 	home.sprite.draw()
 	for i in range(0,len(debrises)):
 		debrises[i].sprite.draw()
+	for i in range(0, len(ants)):
+	 	ants[i].sprite.draw()
 
 # TODO: move this somewhere else
 pyglet.app.run()
